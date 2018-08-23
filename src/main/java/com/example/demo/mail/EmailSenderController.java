1e758adabd4b9edb0f97de8f8a410d6f7e3edb79
yixiang.guo <yixiang.guo@servision.com.cn>
2018-08-23 17:56:04 +0800

    + spring mail test

---
+++ src/main/java/com/example/demo/mail/EmailSenderController.java
@@ -0,0 +1,22 @@
+package com.example.demo.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/mail")
+public class EmailSenderController {
+
+    @Autowired
+    private EmailSenderService emailSenderService;
+
+    @ResponseBody
+    @RequestMapping(value = "/send-to-default", method = RequestMethod.POST)
+    public String test(@RequestBody Map<String, String> param) {
+        emailSenderService.sendToDefault(param.getOrDefault("subject", "test"), param.getOrDefault("html", "test"));
+        return "success";
+    }
+
+}
