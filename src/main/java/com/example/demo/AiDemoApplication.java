f1803be03e1c8ef155cbf456d879c7d45b7ebf14
myKnowing <13681984282@163.com>
2018-09-14 22:43:11 +0800

    修改 spring boot 启动项

--- src/main/java/com/example/demo/AiDemoApplication.java
+++ src/main/java/com/example/demo/AiDemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,6 +10,15 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class AiDemoApplication {
 
     public static void main(String[] args) {
+        /**
+         * 隐藏banner启动方式
+         */
+//        SpringApplication springApplication = new SpringApplication(AiDemoApplication.class);
+//        //设置banner的模式为隐藏
+//        springApplication.setBannerMode(Banner.Mode.OFF);
+//        //启动springboot应用程序
+//        springApplication.run(args);
         SpringApplication.run(AiDemoApplication.class, args);
     }
+
 }
