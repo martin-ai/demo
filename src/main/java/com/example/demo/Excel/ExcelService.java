3321c0b98421e7639febc9c41243b1aaf1a39e58
yixiang.guo <yixiang.guo>
2018-09-18 17:54:58 +0800

    add excel

---
+++ src/main/java/com/example/demo/Excel/ExcelService.java
@@ -0,0 +1,45 @@
+package com.example.demo.Excel;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Service;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class ExcelService {
+
+    private static final String EXCEL_XLS = "xls";
+    private static final String EXCEL_XLSX = "xlsx";
+
+    public void readExcel(String excelFileName) throws Exception {
+        File excelFile = new File(excelFileName);
+        checkExcelValid(excelFile);
+        FileInputStream in = new FileInputStream(excelFile);
+        getWorkbook(in, excelFile);
+    }
+
+    private Workbook getWorkbook(InputStream in, File file) throws IOException {
+        Workbook wb = null;
+        if (file.getName().endsWith(EXCEL_XLS)) {  //Excel 2003
+            wb = new HSSFWorkbook(in);
+        } else if (file.getName().endsWith(EXCEL_XLSX)) {  // Excel 2007/2010
+            wb = new XSSFWorkbook(in);
+        }
+        return wb;
+    }
+
+    private void checkExcelValid(File file) throws Exception {
+        if (!file.exists()) {
+            throw new Exception("文件不存在");
+        }
+        if (!(file.isFile() && (file.getName().endsWith(EXCEL_XLS) || file.getName().endsWith(EXCEL_XLSX)))) {
+            throw new Exception("文件不是Excel");
+        }
+    }
+
+}
