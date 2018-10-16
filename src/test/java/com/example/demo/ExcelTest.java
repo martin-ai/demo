4c452d084bb866dea973fa1170b9954b23667676
yixiang.guo <yixiang.guo>
2018-10-16 13:13:45 +0800

    对传值调用/应用调用的进一步理解

--- src/test/java/com/example/demo/ExcelTest.java
+++ src/test/java/com/example/demo/ExcelTest.java
@@ -2,9 +2,14 @@ package com.example.demo;
 
 import com.example.demo.Excel.ExcelService;
 import org.apache.poi.hssf.record.chart.SeriesToChartGroupRecord;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Iterator;
 
 public class ExcelTest extends AiDemoApplicationTests {
 
@@ -13,8 +18,13 @@ public class ExcelTest extends AiDemoApplicationTests {
 
     @Test
     public void test() throws Exception {
-        String excelFileName = "src/main/resources/info/test.xls";
+        String excelFileName = "src/main/resources/info/MDMM_Database_Structure.xlsx";
         Workbook wb = excelService.readExcel(excelFileName);
+        Sheet sheet = wb.getSheetAt(0);
+        Row row = sheet.getRow(0);
+        for (Cell cell : row) {
+
+        }
         System.out.println(wb);
     }
 
--- src/test/java/com/example/demo/NormalTest.java
+++ src/test/java/com/example/demo/NormalTest.java
@@ -14,38 +14,59 @@ public class NormalTest {
     //3.传值调用：传值调用中传递的参数为基本数据类型，参数视为形参。
     //4.引用调用：传引用调用中，如果传递的参数是引用数据类型，参数视为实参。在调用的过程中，将实参的地址传递给了形参，形参上的改变都发生在实参上。
     public void testCall() {
-        System.out.println("总结1：java的基本数据类型是传值调用，对象引用类型是传引用。");
+        System.out.println("总结1：java的基本数据类型是传值调用，对象引用类型是引用调用。");
         int a = 100;
-        System.out.println(String.format("形参传值调用：参数a 初始值=%d", a));
+        System.out.println(String.format("形参传值调用：参数a 初始值=%d 地址=%d", a, System.identityHashCode(a)));
         callByValue(a);
-        System.out.println(String.format("形参传值调用：参数a 最终值=%d", a));
+        System.out.println(String.format("形参传值调用：参数a 最终值=%d 地址=%d", a, System.identityHashCode(a)));
         System.out.println("总结2：当传值调用时，改变的是形参的值，并没有改变实参的值，实参的值可以传递给形参，但是，这个传递是单向的，形参不能传递回实参。");
 
         String b = "old";
-        System.out.println(String.format("引用调用调用：参数b 初始值=%s", b));
+        System.out.println(String.format("引用调用调用：参数b 初始值=%s 地址=%d", b, System.identityHashCode(b)));
         callByReference1(b);
-        System.out.println(String.format("引用调用调用：参数b 最终值=%s", b));
+        System.out.println(String.format("引用调用调用：参数b 最终值=%s 地址=%d", b, System.identityHashCode(b)));
         System.out.println("总结3：当引用调用时，如果参数是对象，无论对对象做了何种操作，都不会改变实参对象的引用。");
+
         StringBuffer c = new StringBuffer("old");
-        System.out.println(String.format("引用调用调用：参数c 初始值=%s", c));
+        System.out.println(String.format("引用调用调用：参数c 初始值=%s 地址=%d", c, System.identityHashCode(c)));
         callByReference2(c);
-        System.out.println(String.format("引用调用调用：参数c 最终值=%s", c));
+        System.out.println(String.format("引用调用调用：参数c 最终值=%s 地址=%d", c, System.identityHashCode(c)));
         System.out.println("总结4：当引用调用时，如果参数是对象，改变了对象的内容，就会改变实参对象的内容。");
     }
 
     private void callByValue(int a) {
+        int beforeAddress = System.identityHashCode(a);
         a = a + 1;
-        System.out.println(String.format("形参传值调用：参数a 修改值=%d", a));
+        int afterAddress = System.identityHashCode(a);
+        System.out.println(String.format("形参传值调用：参数a 修改值=%d 修改前地址=%d 修改后地址=%d", a, beforeAddress, afterAddress));
     }
 
     private void callByReference1(String b) {
+        int beforeAddress = System.identityHashCode(b);
         b = "new";
-        System.out.println(String.format("引用调用调用：参数b 修改值=%s", b));
+        int afterAddress = System.identityHashCode(b);
+        System.out.println(String.format("引用调用调用：参数b 修改值=%s 修改前地址=%d 修改后地址=%d", b, beforeAddress, afterAddress));
     }
 
     private void callByReference2(StringBuffer c) {
+        int beforeAddress = System.identityHashCode(c);
         c.append("new");
-        System.out.println(String.format("引用调用调用：参数c 修改值=%s", c));
+        int afterAddress = System.identityHashCode(c);
+        System.out.println(String.format("引用调用调用：参数c 修改值=%s 修改前地址=%d 修改后地址=%d", c, beforeAddress, afterAddress));
+    }
+
+    //总结3的面试题
+    protected static class Tst {
+        public void foo(StringBuilder builder) {
+            builder = new StringBuilder("abc");
+        }
+
+        public static void main(String[] args) {
+            Tst t = new Tst();
+            StringBuilder builder = new StringBuilder("def");
+            t.foo(builder);
+            System.out.println(builder.toString());
+        }
     }
 
     @Test
