9f252c6c2ac274de9be87fa07deb2114566b5893
yixiang.guo <yixiang.guo>
2019-02-14 10:56:33 +0800

    + excel poi test

--- src/test/java/com/example/demo/ExcelTest.java
+++ src/test/java/com/example/demo/ExcelTest.java
@@ -1,15 +1,9 @@
 package com.example.demo;
 
 import com.example.demo.Excel.ExcelService;
-import org.apache.poi.hssf.record.chart.SeriesToChartGroupRecord;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Iterator;
 
 public class ExcelTest extends AiDemoApplicationTests {
 
@@ -18,12 +12,15 @@ public class ExcelTest extends AiDemoApplicationTests {
 
     @Test
     public void test() throws Exception {
-        String excelFileName = "src/main/resources/info/MDMM_Database_Structure.xlsx";
+        String excelFileName = "src/main/resources/info/test.xlsx";
         Workbook wb = excelService.readExcel(excelFileName);
         Sheet sheet = wb.getSheetAt(0);
         Row row = sheet.getRow(0);
         for (Cell cell : row) {
-
+            cell.getCellTypeEnum();
+            cell.setCellType(CellType.STRING);
+            RichTextString s = cell.getRichStringCellValue();
+            System.out.println(s.getString());
         }
         System.out.println(wb);
     }
