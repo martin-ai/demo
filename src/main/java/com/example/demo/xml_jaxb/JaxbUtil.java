dbcfc67d6fc98166ddc85602be33544f88c337d4
yixiang.guo <yixiang.guo>
2019-03-27 09:50:11 +0800

    fix

--- src/main/java/com/example/demo/xml_jaxb/JaxbUtil.java
+++ src/main/java/com/example/demo/xml_jaxb/JaxbUtil.java
@@ -14,23 +14,20 @@ public class JaxbUtil {
     //xml转java对象
     @SuppressWarnings("unchecked")
     public static <T> T xmlToBean(String xml, Class<T> c) throws JAXBException {
-        T t = null;
         jaxbContext = JAXBContext.newInstance(c);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        t = (T) unmarshaller.unmarshal(new StringReader(xml));
-        return t;
+        return (T) unmarshaller.unmarshal(new StringReader(xml));
     }
 
     //java对象转xml
     public static String beanToXml(Object obj) throws JAXBException {
-        StringWriter writer = null;
         jaxbContext = JAXBContext.newInstance(obj.getClass());
         Marshaller marshaller = jaxbContext.createMarshaller();
         //Marshaller.JAXB_FRAGMENT:是否省略xml头信息,true省略，false不省略
         marshaller.setProperty(Marshaller.JAXB_FRAGMENT, false);
         marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
-        writer = new StringWriter();
-        marshaller.marshal(obj, writer);
+        StringWriter writer = new StringWriter();
+        marshaller.marshal(obj, new StringWriter());
         return writer.toString();
     }
 
